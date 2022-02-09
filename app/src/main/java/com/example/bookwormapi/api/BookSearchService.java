9d7db786a7e6fc86c317e47244d15ea865288dbb
Andrew Liddle <aliddle98@gmail.com>
2022-02-09 02:28:00 -0800

    Added: - BookSearchService Interface (contains GET for API) - BookRepository (used toi interact with the Google API)

---
+++ app/src/main/java/com/example/bookwormapi/api/BookSearchService.java
@@ -0,0 +1,15 @@
+package com.example.bookwormapi.api;
+
+import com.example.bookwormapi.VolumesResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface BookSearchService {
+    @GET("/books/v1/volumes")
+    Call<VolumesResponse> searchVolumes(
+            @Query("q") String query,
+            @Query("inauthor") String author,
+            @Query("key") String apiKey
+    );
+}
